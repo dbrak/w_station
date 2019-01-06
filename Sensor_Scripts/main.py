f0b5a39c3a23f47f76bcec2551aaa7764fc86f00
Jbrak <joe@brakoniecki.com>
2019-01-06 14:40:48 +0000

    reformated file

--- Sensor_Scripts/main.py
+++ Sensor_Scripts/main.py
@@ -1,14 +1,11 @@
 import time
 from gc_CRUD import gcWrite
-# import socket
 
 import pigpio
 import DHT22
 
 pi = pigpio.pi()
 s = DHT22.sensor(pi, 4)
-# hostname = str(socket.gethostname())
-
 
 while True:
 
@@ -18,11 +15,11 @@ while True:
 
     if (tm % 5 == 0) and (ts == 0):
         s.trigger()
-        time.sleep(0.05)
+        time.sleep(0.03)
         t = s.temperature()
         h = s.humidity()
 
         i = time.strftime("%y:%m:%d %H:%M")
 
-        gcWrite("Joe's Office", t, h, i)
+        gcWrite('Joe`s Office', t, h, i)
         print(tm, ts, "|", i, "|", time.strftime("%H:%M:%S"))
