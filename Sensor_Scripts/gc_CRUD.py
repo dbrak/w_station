1f11e26b45d2b1a72375b4b648f86e8a6e046314
Jbrak <joe@brakoniecki.com>
2019-01-07 20:26:00 +0000

    added failsafe

--- Sensor_Scripts/gc_CRUD.py
+++ Sensor_Scripts/gc_CRUD.py
@@ -23,7 +23,7 @@ def gcWrite(location,temp,humid,timestamp):
         db.collection(str(location)). document(x).set(timestamp)
 
     except:
-        print("Firebase write failed at: " + timestamp)
+        print("Firebase write failed at: " + str(timestamp))
 
 
 def gcRead(location, timestamp):
