82257391f4c09987caf5609f918b214cdd3ad8ae
Jbrak <joe@brakoniecki.com>
2019-01-08 18:32:40 +0000

    Reverted to orinal and added sleep statment

--- Sensor_Scripts/main.py
+++ Sensor_Scripts/main.py
@@ -5,29 +5,27 @@ import json
 import pigpio
 import DHT22
 
-ti = time.gmtime()
-ts = int(time.strftime('%S', ti))
-tm = int(time.strftime('%M', ti))
+with open('config.json','r') as f:
+    config = json.load(f)
 
-
-def main():
-    with open('config.json', 'r') as f:
-        config = json.load(f)
-
-    pi = pigpio.pi()
-    s = DHT22.sensor(pi, 4)
-
-    s.trigger()
-    time.sleep(0.03)
-    t = s.temperature()
-    h = s.humidity()
-
-    i = time.strftime("%y:%m:%d %H:%M")
-
-    gcWrite(config['location'], t, h, i)
-    print(tm, ts, "|", i, "|", time.strftime("%H:%M:%S"))
-
-t = ["5","10","15","20","25","30","35","40","45","50","55","60"]
+pi = pigpio.pi()
+s = DHT22.sensor(pi, 4)
 
 while True:
-    schedule.every().hours.at(t).do(main())
+    ti = time.gmtime()
+    ts = int(time.strftime('%S', ti))
+    tm = int(time.strftime('%M', ti))
+
+    if (tm % 5 == 0) and (ts == 0):
+        s.trigger()
+        time.sleep(0.03)
+        t = s.temperature()
+        h = s.humidity()
+
+        i = time.strftime("%y:%m:%d %H:%M")
+
+        gcWrite(config['location'], t, h, i)
+        print(tm, ts, "|", i, "|", time.strftime("%H:%M:%S"))
+
+        time.sleep(250)
+
