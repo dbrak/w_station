6229dbdadc725f65d8a0bc02323937fceae331e0
Jbrak <joe@brakoniecki.com>
2019-01-24 19:45:21 +0000

    Commited File

---
+++ Bokeh/Query.py
@@ -0,0 +1,33 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import json
+
+with open('Cred','r') as f:
+    config = json.load(f)
+
+# Use a service account
+cred = credentials.Certificate(config['Firebase'])
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+def query(location, type, operation, value, field):
+
+    collection = db.collection(str(location))
+    q = collection.where(str(type), str(operation), value).get()
+
+    dictionary = {el.id: el.to_dict() for el in q}
+
+    data = {}
+
+    for i in dictionary.keys():
+
+        k = dictionary[i]
+        t = k[str(field)]
+        data.update({i : t})
+    return data
+
+#temp = query('Joe\'s Study', 'Temperature', '>', 20, 'Temperature')
+
+#print (temp)
